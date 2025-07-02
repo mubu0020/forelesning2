676e94864e14c822d2774e44c797aa65a922b101
mubu002 <mustafa_buga1@hotmail.com>
2025-07-02 20:52:46 +0200

    Opprettet css fil, for å få det til å se noe bedre ut, og endret på index.jsx fil, slik at dette er kun en begynnelse

--- client/index.jsx
+++ client/index.jsx
@@ -5,8 +5,32 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+function FrontPage(){
+    return (
+        <>
+            <h2>Welcome</h2>
+            <a href={"#"}>Ask a question</a>
+        </>
+    );
+}
+
 function Application() {
-    return <h1>This is the end</h1>;
+    return (
+     <>
+        <header>
+            <h1>The Quiz App</h1>
+        </header>
+         <nav>
+             <a href={""}>Front Page</a>
+         </nav>
+         <main>
+             <FrontPage />
+         </main>
+         <footer>
+             Created by Mustafa
+         </footer>
+     </>
+    );
 }
 
 root.render(<Application />)
