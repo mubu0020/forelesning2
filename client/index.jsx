7b10466db1660d70b6d05175ec314107c81fbd59
mubu002 <mustafa_buga1@hotmail.com>
2025-07-02 16:57:39 +0200

    Endret dev i package.json fra watch til serve, slik at jeg får en adresse for å se applikasjon i realtime

---
+++ client/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Application() {
+    return <h1>This is the end</h1>;
+}
+
+root.render(<Application />)
